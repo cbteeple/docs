520b5732ad4de038fa9b320b9191ae198921d960
Clark Teeple <cbteeple@gmail.com>
2019-11-16 13:33:06 -0500

    back to original svg script

--- assets/js/svg-inline.js
+++ assets/js/svg-inline.js
@@ -1,9 +1,12 @@
-/*$(document).ready(readyFn);
+$(document).ready(readyFn);
 
 
 function readyFn(jQuery) {
 
-    // Replace SVG images with inline
+}
+
+
+// Replace SVG images with inline
     jQuery('img').filter(function () {
         return this.src.match(/.*\.svg$/);
     }).each(function () {
@@ -38,10 +41,6 @@ function readyFn(jQuery) {
 
     });
 
-    nav_bar();
-
-}
-
 
 function removeStyles(el) {
     el.removeAttr('style');
@@ -53,31 +52,3 @@ function removeStyles(el) {
 
 }
 
-*/
-
-
-
-/*  Replace all SVG images with inline SVG */
-$(\'img.svg\').each(function(){
-    var $img = $(this);
-    var imgID = $img.attr(\'id\');
-    var imgClass = $img.attr(\'class\');
-    var imgURL = $img.attr(\'src\');
-
-    $.get(imgURL, function(data) {
-         Get the SVG tag, ignore the rest
-        var $svg = $(data).find(\'svg\');
-         Add replaced image\'s ID to the new SVG
-        if (typeof imgID !== \'undefined\') {
-            $svg = $svg.attr(\'id\', imgID);
-        }
-         Add replaced image\'s classes to the new SVG
-        if (typeof imgClass !== \'undefined\') {
-            $svg = $svg.attr(\'class\', imgClass+\' replaced-svg\');
-        }
-         Remove any invalid XML tags as per http:validator.w3.org
-        $svg = $svg.removeAttr(\'xmlns:a\');
-         Replace image with new SVG
-        $img.replaceWith($svg);
-    });
-});
