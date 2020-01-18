c0621e3c8b7bc12fe7517a70ce667d7a0ae77857
cbteeple <cbteeple@g.harvard.edu>
2020-01-18 18:18:49 -0500

    Update bash files

--- assets/files/update_repos.sh
+++ assets/files/update_repos.sh
@@ -1,9 +1,18 @@
+#!/bin/bash
 # If you send an argument, use it. Otherwise, use the default list of repos
-if [ "$1" != "" ]; then
+if [ "$1" != "" ] && [ "$1" != "-a" ]; then
     declare -a StringArray=("$1")
 else
     declare -a StringArray=("docs" "pressure_controller_docs" "hand_arm_cbt_docs")
 fi
+
+# Ask user for a commit message unless you use the -u input
+if [ "$2" != "-u" ]; then
+    read -p "Commit Message: " desc
+else
+    desc="Update Documentation"
+fi
+
 
 # Iterate over the repo list
 for val in ${StringArray[@]}; do
@@ -11,12 +20,15 @@ for val in ${StringArray[@]}; do
 	if [[ -d $val ]]
 	then
 		echo 
-		echo Updating Remote:  "$val"
+		echo UPDATING REMOTE:
+		echo repo:    "$val"
+		echo msg : "$desc"
+		echo 
 
 		# Go into the repo's main folder, commit, then go back out
 		cd $val
 		git add .
-		git commit -m 'Update Documentation'
+		git commit -m "$desc"
 		git push
 		cd ..
 	else
